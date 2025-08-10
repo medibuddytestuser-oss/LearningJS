2a9606b377023ee7c2c45f0c0c9b5a139b2517b5
medibuddytestuser-oss <medibuddy.testuser@gmail.com>
2025-08-10 05:03:49 +0000

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LearningJS
+
+
+// Documents
+// https://tc39.es/ecma262/#sec-syntax-directed-operations
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
---
+++ basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"
+
+console.log(3+3);
+console.log("ashish");
+
+console.log("01" +" " +"ashish");
+
+// Documents
+// https://tc39.es/ecma262/#sec-syntax-directed-operations
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
